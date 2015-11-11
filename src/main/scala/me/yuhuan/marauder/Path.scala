b7b71cea7ba436f0fee9e2949b4f71169bd98fb1
yuhuan <jyuhuan@gmail.com>
2015-11-10 23:56:06 -0500

    changed to friendlier names

--- src/main/scala/me/yuhuan/marauder/Path.scala
+++ src/main/scala/me/yuhuan/marauder/Path.scala
@@ -6,31 +6,31 @@ import scala.collection.mutable
   * @author Yuhuan Jiang (jyuhuan@gmail.com).
   */
 trait Path[+S, +A] { outer ⇒
-  def ss: IndexedSeq[S]
-  def as: IndexedSeq[A]
+  def states: IndexedSeq[S]
+  def actions: IndexedSeq[A]
 
-  def length = as.length
+  def length = actions.length
 
   def mapState[S1](f: S ⇒ S1) = new Path[S1, A] {
-    def ss: IndexedSeq[S1] = outer.ss.map(f)
-    def as: IndexedSeq[A] = outer.as
+    def states: IndexedSeq[S1] = outer.states.map(f)
+    def actions: IndexedSeq[A] = outer.actions
   }
 
   def mapAction[A1](f: A ⇒ A1) = new Path[S, A1] {
-    def ss: IndexedSeq[S] = outer.ss
-    def as: IndexedSeq[A1] = outer.as.map(f)
+    def states: IndexedSeq[S] = outer.states
+    def actions: IndexedSeq[A1] = outer.actions.map(f)
   }
 
   def compressed: Path[S, A] = {
     val newStates = mutable.ArrayBuffer[S]()
     val newActions = mutable.ArrayBuffer[A]()
 
-    var lastState: S = ss(0)
+    var lastState: S = states(0)
     newStates += lastState
 
-    for (i ← 1 until ss.length) {
-      val curState = ss(i)
-      val curAction = as(i - 1)
+    for (i ← 1 until states.length) {
+      val curState = states(i)
+      val curAction = actions(i - 1)
 
       if (curState != lastState) {
         newStates += curState
@@ -43,21 +43,21 @@ trait Path[+S, +A] { outer ⇒
 
   override def toString = {
     val sb = new StringBuilder()
-    sb append ss(0)
-    for (i ← 1 until ss.length) {
-      sb append as(i - 1)
-      sb append ss(i)
+    sb append states(0)
+    for (i ← 1 until states.length) {
+      sb append actions(i - 1)
+      sb append states(i)
     }
     sb.toString
   }
 }
 
 object Path {
-  def apply[S, A](states: IndexedSeq[S], actions: IndexedSeq[A]) = {
-    require(states.length == actions.length + 1)
+  def apply[S, A](_states: IndexedSeq[S], _actions: IndexedSeq[A]) = {
+    require(_states.length == _actions.length + 1)
     new Path[S, A] {
-      def ss: IndexedSeq[S] = states
-      def as: IndexedSeq[A] = actions
+      def states: IndexedSeq[S] = _states
+      def actions: IndexedSeq[A] = _actions
     }
   }
 }
@@ -65,8 +65,8 @@ object Path {
 object Test {
   def main(args: Array[String]) {
     val p = new Path[String, Int] {
-      def ss: IndexedSeq[String] = Array("NP", "VP", "VP", "VP", "ADJP", "NNP", "NNP", "NNP", "SBAR")
-      def as: IndexedSeq[Int] = Array(0, 1, 2, 3, 4, 5, 6, 7)
+      def states: IndexedSeq[String] = Array("NP", "VP", "VP", "VP", "ADJP", "NNP", "NNP", "NNP", "SBAR")
+      def actions: IndexedSeq[Int] = Array(0, 1, 2, 3, 4, 5, 6, 7)
     }
 
     val pc = p.compressed
